a55d5c7a4d70dbd16e864d13dbd598450bf2cda9
CLD200-a00 <cld200-a00@education.cloud.sap>
2020-07-02 12:03:26 +0000

    running ex11

---
+++ unit-tests/src/test/java/cld200/a00/UnitTest.java
@@ -0,0 +1,12 @@
+package cld200.a00;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
